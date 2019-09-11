28392e17aa96049853dd3250c5c6064753f97990
KipinskiPiotr <49042374+KipinskiPiotr@users.noreply.github.com>
2019-09-11 17:32:00 +0200

    Update serialServos.ino

--- serialServos.ino
+++ serialServos.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-Servo servos[4];
+Servo servos[5];
 String data = "nothing";
 
 void moveServo(int num, int angle){
@@ -19,11 +19,13 @@ void setup() {
   servos[0].attach(3);
   servos[0].write(90);
   servos[1].attach(5);
-  servos[1].write(156); //115 + 41
+  servos[1].write(90);
   servos[2].attach(6);
-  servos[2].write(128);
+  servos[2].write(90);
   servos[3].attach(9);
-  servos[3].write(52);
+  servos[3].write(90);
+  servos[4].attach(10);
+  servos[4].write(90);
   Serial.begin(115200);
 }
 
